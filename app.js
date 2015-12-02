7ac51f61100ae5a7bb960d3c3f572a48bccc1410
Johannes Larsson <mrja1lisepic@gmail.com>
2015-12-02 14:10:57 +0100

    cleaned up app.js

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
+/* global angular */
 angular.module('app', ['ngRoute'])
 
-.controller('NavController', function($scope, $route, $location) {
-   $scope.isActive = function(href) {
-       console.log('location: ', $location.url());
-       if (href == $location.url()) return 'active';
-       else return '';
-   };
-   
-   $scope.contributors = [
-	{ name: 'Johannes', link: 'http://github.com/johannes-larsson' },
-	{ name: 'Tom', link: 'http://github.com/indietom' }
-	];
+.controller('NavController', function ($scope, $route, $location) {
+  $scope.isActive = function (href) {
+    console.log('location: ', $location.url())
+    if (href === $location.url()) return 'active'
+    else return ''
+  }
+
+  $scope.contributors = [
+    { name: 'Johannes', link: 'http://github.com/johannes-larsson' },
+    { name: 'Tom', link: 'http://github.com/indietom' }
+  ]
 })
 
-.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-    $routeProvider
-    .when('/home',  { templateUrl: 'views/home.html'  })
-    .when('/about', { templateUrl: 'views/about.html' })
-    .otherwise('/home');
-    
-    //$locationProvider.html5Mode({enabled: true, requireBase: false});
+.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/home', { templateUrl: 'views/home.html' })
+  .when('/about', { templateUrl: 'views/about.html' })
+  .otherwise('/home')
+
+  // $locationProvider.html5Mode({enabled: true, requireBase: false});
 }])
